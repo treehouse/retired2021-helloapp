e5ae271038a6d65483b8f06f558742bd75d05cc1
thatismatt <thatismatt@gmail.com>
2009-08-11 17:20:52 +0100

    Adding the LINQ to SQL classes and the skeleton of the bot.

--- HelloBot/Program.cs
+++ HelloBot/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Linq;
 using System.Linq;
 using System.Text;
-using System.Xml.Linq;
+using Hello.Repo;
+using Dimebrain.TweetSharp.Fluent;
+using Dimebrain.TweetSharp.Extensions;
 
 namespace Hello.Bot
 {
@@ -10,7 +14,60 @@ namespace Hello.Bot
     {
         public static void Main(string[] args)
         {
-            // Do the bot thing...
+            ProcessDirectMessages();
+
+            ProcessMentions();
+
+            ProcessHashTag();
+        }
+
+        private static IFluentTwitter GetTwitterRequest()
+        {
+            return FluentTwitter.CreateRequest()
+                                .AuthenticateAs(
+                                    ConfigurationManager.AppSettings["TwitterBotUsername"],
+                                    ConfigurationManager.AppSettings["TwitterBotPassword"]);
+        }
+
+        private static void ProcessHashTag()
+        {
+            var statusesWithOurHashTag = GetTwitterRequest()
+                .Search()
+                .Query()
+                .ContainingHashTag(ConfigurationManager.AppSettings["TwitterHashTag"])
+                //.Since(123456789)
+                //.Skip(2)
+                .Take(int.MaxValue)
+                .Request()
+                .AsSearchResult();
+        }
+
+        private static void ProcessMentions()
+        {
+            var statusesMentioningMe = GetTwitterRequest()
+                .Statuses()
+                .Mentions()
+                //.Since(123456789)
+                .Request()
+                .AsStatuses();
+        }
+
+        private static void ProcessDirectMessages()
+        {
+            var receivedDMs = GetTwitterRequest()
+                .DirectMessages()
+                .Received()
+                //.Since()
+                .Request()
+                .AsDirectMessages();
+        }
+
+        private static void SendDirectMessage(string screenName, string message)
+        {
+            var response = GetTwitterRequest()
+                .Statuses()
+                .Update(String.Format("d {0} {1}", screenName, message))
+                .Request();
         }
     }
 }
