c7d94a808c2efe864270287e5fca03cc1259eeb7
thatismatt <thatismatt@gmail.com>
2009-08-12 12:31:05 +0100

    Added SQL data files, generated by SQL Data Compare.

---
+++ Hello.Repo/SQL/Data/dbo.Events_Data.sql
@@ -0,0 +1,3 @@
+SET IDENTITY_INSERT [dbo].[Events] ON
+INSERT INTO [dbo].[Events] ([EventID], [Name], [Slug]) VALUES (1, 'Test Event', 'testevent')
+SET IDENTITY_INSERT [dbo].[Events] OFF
---
+++ Hello.Repo/SQL/Data/dbo.UserTypes_Data.sql
@@ -0,0 +1,3 @@
+INSERT INTO [dbo].[UserTypes] ([UserTypeID], [EventID], [Name], [DefaultColour], [BurningColour], [HotColour], [WarmColour]) VALUES ('biz', 1, N'Business Person', '0000cc', '0000ff', '0000ee', '0000dd')
+INSERT INTO [dbo].[UserTypes] ([UserTypeID], [EventID], [Name], [DefaultColour], [BurningColour], [HotColour], [WarmColour]) VALUES ('des', 1, N'Designer', '00cc00', '00ff00', '00ee00', '00dd00')
+INSERT INTO [dbo].[UserTypes] ([UserTypeID], [EventID], [Name], [DefaultColour], [BurningColour], [HotColour], [WarmColour]) VALUES ('dev', 1, N'Developer', 'cc0000', 'ff0000', 'ee0000', 'dd0000')
