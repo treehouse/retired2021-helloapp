0b96e09a7f2bc41731a1d92e575139811084331c
thatismatt <thatismatt@gmail.com>
2009-08-17 22:32:56 +0800

    Removed unused routes config,

--- Hello.App/Global.asax.cs
+++ Hello.App/Global.asax.cs
@@ -17,24 +17,6 @@ namespace HelloApp
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(null,
-                "user/random{format}",
-                new { controller = "User", action = "Random" }
-                //, new { username = "[fdas]+" }
-            );
-
-            //routes.MapRoute(null,
-            //    "user/{username}",
-            //    new { controller = "User", action = "Index", format = "html" }
-            //    //, new { username = "[fdas]+" }
-            //);
-
-            routes.MapRoute(null,
-                "{eventslug}",
-                new { controller = "Event", action = "Index" },
-                new { eventslug = @"[0-9a-fA-F\-]+" }
-            );
-
-            routes.MapRoute(null,
                 "{controller}/{action}/{id}",
                 new { controller = "Home", action = "Index", id = "" }
             );
