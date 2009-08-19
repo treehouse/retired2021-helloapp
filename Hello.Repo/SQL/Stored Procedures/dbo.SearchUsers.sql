74578d8271a8f2852288a030545cc4d0fffb7127
MelvynHarbour <melharbour@googlemail.com>
2009-08-19 10:40:32 +0100

    Update to search and cleanup data

--- Hello.Repo/SQL/Stored Procedures/dbo.SearchUsers.sql
+++ Hello.Repo/SQL/Stored Procedures/dbo.SearchUsers.sql
@@ -1,3 +1,4 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
@@ -31,27 +32,44 @@ BEGIN
 
 	-- Strongly weight exact match of username
 	INSERT INTO #TempResults ( Username, Weighting )
-	SELECT u.Username, 50
+	SELECT u.Username, 10
 	FROM dbo.Users u
 	INNER JOIN #SearchTerms t ON u.Username = t.Term
 	WHERE u.ShadowAccount = 0
 	
 	-- Then LIKE match
 	INSERT INTO #TempResults (Username, Weighting)
-	SELECT u.Username, 30
+	SELECT u.Username, 10
 	FROM dbo.Users u
 	INNER JOIN #SearchTerms t ON u.Username LIKE '%' + t.Term + '%' AND u.Username <> t.Term
+	WHERE u.ShadowAccount = 0
+	
+	INSERT INTO #TempResults ( Username, Weighting )
+	SELECT u.Username, 10
+	FROM dbo.Users u
+	INNER JOIN dbo.UserTypes ut ON u.UserTypeID = ut.UserTypeID
+	INNER JOIN #SearchTerms t ON ut.Name = t.Term
+	WHERE u.ShadowAccount = 0
+	
+	INSERT INTO #TempResults (Username, Weighting)
+	SELECT u.Username, 10
+	FROM dbo.Users u
+	INNER JOIN dbo.Tags t ON u.Username = t.Username
+	INNER JOIN #SearchTerms st ON t.Name = st.Term
 	WHERE u.ShadowAccount = 0
 
     -- Insert statements for procedure here
 	SELECT u.Username, u.ImageURL, u.Created, u.Updated, u.UserTypeID, u.ShadowAccount
 	FROM dbo.Users u
 	INNER JOIN #TempResults t ON u.Username = t.Username
-	ORDER BY t.Weighting DESC
+	GROUP BY u.Username, u.ImageURL, u.Created, u.Updated, u.UserTypeID, u.ShadowAccount
+	HAVING SUM(t.Weighting) >= (SELECT 10 * COUNT(*) FROM #SearchTerms)
+	ORDER BY SUM(t.Weighting) DESC
 	
 	DROP TABLE #TempResults
 	DROP TABLE #SearchTerms
 END
 GO
+
 GRANT EXECUTE ON  [dbo].[SearchUsers] TO [hello_web]
 GO
