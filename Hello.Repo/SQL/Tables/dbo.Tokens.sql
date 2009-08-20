0a6821ea7cd19497c78c6ab82d7e86caebe57747
thatismatt <thatismatt@gmail.com>
2009-08-20 09:52:31 +0100

    Rename Token.Token to Token.Code in SQL, as well as DataContext.

--- Hello.Repo/SQL/Tables/dbo.Tokens.sql
+++ Hello.Repo/SQL/Tables/dbo.Tokens.sql
@@ -2,16 +2,17 @@ CREATE TABLE [dbo].[Tokens]
 (
 [TokenID] [int] NOT NULL IDENTITY(1, 1),
 [CampaignID] [int] NOT NULL,
-[Token] [varchar] (10) COLLATE Latin1_General_CI_AS NOT NULL,
+[Code] [varchar] (10) COLLATE Latin1_General_CI_AS NOT NULL,
 [AllowedRedemptions] [int] NOT NULL CONSTRAINT [DF_Tokens_AllowedRedemptions] DEFAULT ((1))
 )
 
+GO
+ALTER TABLE [dbo].[Tokens] ADD CONSTRAINT [CX_Token] UNIQUE NONCLUSTERED  ([Code])
+
+
 
 GO
-ALTER TABLE [dbo].[Tokens] ADD CONSTRAINT [CX_Token] UNIQUE NONCLUSTERED  ([Token])
 
-
-GO
 ALTER TABLE [dbo].[Tokens] ADD CONSTRAINT [PK_Tokens] PRIMARY KEY CLUSTERED  ([TokenID]) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Tokens] ADD CONSTRAINT [FK_Tokens_Campaigns] FOREIGN KEY ([CampaignID]) REFERENCES [dbo].[Campaigns] ([CampaignID])
