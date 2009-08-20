96ffe2b67727dbdd0141c29fe69c753385c6eb85
benadderson <ben.adderson@googlemail.com>
2009-08-20 10:37:32 +0100

    pulled out common setup code to a helper method

---
+++ Hello.Tests/TestHelperMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hello.Repo;
+
+namespace Hello.Tests
+{
+    public static class TestHelperMethods
+    {
+        public static void SetupTestEvent(HelloRepoDataContext repo)
+        {
+            repo.Events.InsertOnSubmit(new Event
+            {
+                Start = DateTime.Now,
+                End = DateTime.Now.AddDays(1),
+                HiFiveLimit = 5,
+                Slug = "testEvent",
+                Name = "testEvent"
+            });
+            repo.SubmitChanges();
+        }
+    }
+}
